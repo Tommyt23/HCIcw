62eb17018cb65db6164902e4b8cc69086afd7a70
Tom Thurston <t.thurston@students.reading.ac.uk>
2024-02-21 22:21:08 +0000

    add routes between pages

--- website/src/App.js
+++ website/src/App.js
@@ -1,12 +1,17 @@
 import './App.scss';
 import {Routes, Route} from "react-router-dom";
 import Layout from './components/Layout';
+import Email from "./components/Email";
+import Valorant from "./components/Valorant";
 
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />} />
+      <Route path="/Email" element={<Email />} />
+      <Route path="/Valorant" element={<Valorant />} />
+      <Route path="/CS2" element={<CS2 />} />
     </Routes>
     );
 }
