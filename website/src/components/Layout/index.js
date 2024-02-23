b385319ad20ad7c532f927409c37e49042545c31
Josh Alcott <rb011964@student.reading.ac.uk>
2024-02-23 11:01:00 +0000

    added a slide show

--- website/src/components/Layout/index.js
+++ website/src/components/Layout/index.js
@@ -5,6 +5,29 @@ import valorant_esports from"../../assets/images/valorant_esports.png"
 import cs2 from "../../assets/images/cs2.jpg"
 
 class HomePage extends React.Component {
+    componentDidMount() {
+        this.showSlides(1);
+    }
+    showSlides(slideIndex) {
+        let i;
+        const slides = document.getElementsByClassName("slideshow");
+        const dots = document.getElementsByClassName("dot");
+        if (slideIndex > slides.length) {
+            slideIndex = 1;
+        }
+        if (slideIndex < 1) {
+            slideIndex = slides.length;
+        }
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" active", "");
+        }
+        slides[slideIndex - 1].style.display = "block";
+        dots[slideIndex - 1].className += " active";
+        setTimeout(() => this.showSlides(slideIndex + 1), 2000); // Change slide every 2 seconds
+    }
     render(){
         return(
             <>
@@ -21,6 +44,10 @@ class HomePage extends React.Component {
                     </img>
                     </div>
             </div>
+                <div>
+                    <span class="dot" onclick="currentSlide(1)"></span>
+                    <span class="dot" onclick="currentSlide(2)"></span>
+                </div>
     </div>
             </>
 
