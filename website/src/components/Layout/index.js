6a54630dae079918bb5009c39f478c1ba782b6fe
Josh Alcott <rb011964@student.reading.ac.uk>
2024-02-23 01:21:17 +0000

    jj

--- website/src/components/Layout/index.js
+++ website/src/components/Layout/index.js
@@ -2,6 +2,7 @@ import React from "react"
 import './index.scss';
 import Sidebar from '../Sidebar';
 import valorant_esports from"../../assets/images/valorant_esports.png"
+import cs2 from "../../assets/images/cs2.jpg"
 
 class HomePage extends React.Component {
     render(){
@@ -10,11 +11,17 @@ class HomePage extends React.Component {
             <Sidebar />
             <div className="Header">
                 <p>Welcome to Student Esports society </p>
-                <div className="slideshow">
-                    <img src={valorant_esports} alt="valorant esports">
-                    </img>
-                   </div>
                 </div>
+            <div className="slideshow-container">
+            <div className="slideshow">
+                <img src={valorant_esports} alt="valorant esports">
+                </img>
+                <div class="slideshow">
+                    <img src={cs2} alt="cs2">
+                    </img>
+                    </div>
+            </div>
+    </div>
             </>
 
         );
