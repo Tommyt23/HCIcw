18847900586e5130d3e4e63fdc3d60a1da106849
Gabriel Dorosh <gabsdorosh@gmail.com>
2024-02-24 16:22:50 +0000

    - Added Header Component with customisable name tag - Added Recent Matches component with customisable match data input 	- Added Match Details Overlay component to go alongside that (Kinda ugly rn) - Added Hero Banner component but ended up stripping it so it is literally just an image - Changed sidebar from `position:absolute` to `position: fixed` in order to stop the sidebar from overlapping with the header. - Added `min-width: 100px;` to the sidebar so it is usable on mobile. - Changed the default font to OpenDyslexic-Regular

--- website/src/components/CS2/index.js
+++ website/src/components/CS2/index.js
@@ -1,11 +1,83 @@
-import Sidebar from "../Sidebar";
+import React, {useState} from 'react';
+import Header from '../Header';
+import HeroBanner from '../HeroBanner';
+import RecentMatches from '../RecentMatches';
+import MatchDetailsOverlay from '../MatchDetailsOverlay';
+import '../../index.css'
+import './index.scss';
 
 const CS2 = () => {
-    return(
-        <>
-        <Sidebar />
-        </>
-        )
+    const title = 'Counter-Strike 2';
+    const [selectedMatch, setSelectedMatch] = useState(null);
+
+    const handleMatchCardClick = (matchId) => {
+        setSelectedMatch(matchId);
+    };
+
+    // Temporary static data for recent pro matches
+    const recentMatchesData = [
+        {
+            team1: 'HEROIC',
+            team2: 'GamerLegion',
+            date: '22-02-2024',
+            type: 'Best of 3',
+            event: 'PGL CS2 Major Copenhagen 2024 Europe RMR B',
+            score: '2-0',
+            id: 1
+        },
+        {
+            team1: 'ECSTATIC',
+            team2: 'Guild Eagles',
+            date: '22-02-2024',
+            type: 'Best of 3',
+            event: 'PGL CS2 Major Copenhagen 2024 Europe RMR B',
+            score: '2-1',
+            id: 2
+        },
+        {
+            team1: 'ENCE',
+            team2: 'Astralis',
+            date: '22-02-2024',
+            type: 'Best of 3',
+            event: 'PGL CS2 Major Copenhagen 2024 Europe RMR B',
+            score: '2-0',
+            id: 3
+        },
+        {
+            team1: 'Natus Vincere',
+            team2: 'G2 Esports',
+            date: '22-02-2024',
+            type: 'Best of 3',
+            event: 'PGL CS2 Major Copenhagen 2024 Europe RMR B',
+            score: '2-1',
+            id: 4
+        }
+    ];
+
+    return (
+        <div>
+            <Header title={title} />
+            <main>
+                <HeroBanner />
+                <RecentMatches
+                    onMatchCardClick={handleMatchCardClick}
+                    recentMatchesData={recentMatchesData}
+                />
+                {selectedMatch && (
+                    <MatchDetailsOverlay match={recentMatchesData.find(
+                        (match) => match.id === selectedMatch
+                    )}
+                    onClose={() => setSelectedMatch(null)}
+                />
+                )}
+                {/*
+                    <Stats />
+                    <News />
+                    <Footer />
+                */}
+            </main>
+        </div>
+    );
 };
 
 export default CS2;
---
+++ website/src/components/HeroBanner/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './index.scss';
+
+const HeroBanner = () => {
+    return (
+        <div className="hero-banner">
+            {/*
+            <h2>Welcome to Counter-Strike 2</h2>
+            <p>Counter-Strike 2 is a first-person shooter developed by Valve.</p>
+            */}
+        </div>
+    );
+};
+
+export default HeroBanner;
