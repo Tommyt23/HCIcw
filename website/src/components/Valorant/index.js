63af8a51f31b39ca8db8680b84a24fd7a04c9bf5
Josh Alcott <rb011964@student.reading.ac.uk>
2024-02-28 18:36:46 +0000

    upadated photos for valo page

--- website/src/components/Valorant/index.js
+++ website/src/components/Valorant/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import Sidebar from "../Sidebar";
 import "./valorant.scss";
-import valorant_esports from "../../assets/images/valorant_esports.png";
+import ValorantHero from "../../assets/images/ValorantHero.png";
 import breeze from "../../assets/images/breeze.png";
+import sunset from "../../assets/images/sunset.png";
 import Header from "../Header";
 const Valorant = () => {
     const title = 'Valorant';
@@ -12,9 +13,9 @@ const Valorant = () => {
         <div>
             <Header title={title} />
         </div>
-        {/*<div className="valo">*/}
-        {/*    <img src={valorant_esports} alt="valorant esports" />*/}
-        {/*    </div>*/}
+        <div className="valo">
+            <img src={ValorantHero} alt="valorant esports" />
+            </div>
         <div className = "container">
             <img src={breeze} alt="breeze"></img>
             <div className="Teams">Furia vs NRG</div>
@@ -22,6 +23,14 @@ const Valorant = () => {
             <div className="Type">Type: Best of 3</div>
             <div className="score">Score: 0-2</div>
             <div className="event">Event: vct americas</div>
+        </div>
+        <div className = "container2">
+            <img src={sunset} alt="sunset"></img>
+            <div className="Teams">FNATIC vs Team Vitality</div>
+            <div className="Date">Date: 23/2/2024</div>
+            <div className="Type">Type: Best of 3</div>
+            <div className="score">Score: 2-0</div>
+            <div className="event">Event: VCT EMEA</div>
         </div>
         </>
         )
